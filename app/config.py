aee7769968621d3d6097052edcd3536001ecd5b6
Med00amine <boussoffara09@gmail.com>
2026-07-25 08:42:25 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,6 +5,17 @@ load_dotenv()  # Load environment variables from .env file
 
 # Access environment variables
 MODEL_PATH = os.getenv("MODEL_PATH")
-API_TITLE = os.getenv("API_TITLE")
-API_VERSION = os.getenv("API_VERSION")
-LOG_LEVEL = os.getenv("LOG_LEVEL")
+API_TITLE = os.getenv(
+    "API_TITLE",
+    "Customer Churn Prediction API"
+)
+
+API_VERSION = os.getenv(
+    "API_VERSION",
+    "1.0.0"
+)
+
+LOG_LEVEL = os.getenv(
+    "LOG_LEVEL",
+    "INFO"
+)
